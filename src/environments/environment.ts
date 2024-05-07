21c519fd27a5ffcad6e4f972bc774c4f04de972d
hernandezcarlos <carher88@gmail.com>
2024-05-07 00:01:05 -0600

    Add package server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://acmc-backend-production.up.railway.app/api/',
+  apiUrl: 'https://quercu-server-production.up.railway.app/',
 };
